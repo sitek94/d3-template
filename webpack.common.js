b2befb6e0c7a7f1bb443a01023b3fc351f2b54ec
Maciek Sitkowski <msitkowski94@gmail.com>
2020-09-08 16:01:44 +0200

    Add prod and dev config

--- webpack.common.js
+++ webpack.common.js
@@ -5,7 +5,6 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main-[contentHash].js',
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
